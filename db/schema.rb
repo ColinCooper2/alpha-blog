0a9f7453293b85f2bfd97e0bf029333ad5bc5688
Colin Cooper <cooperfamily@wowway.com>
2016-06-26 22:56:27 -0400

    create new template

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626224726) do
+ActiveRecord::Schema.define(version: 20160626231949) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
